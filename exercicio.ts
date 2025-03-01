fa9c466b8b23e1563ae355fa9f613ab4b064331e
Lucas Vitareli <160258323+lucasvitareli@users.noreply.github.com>
2025-03-01 12:54:03 +0000

    Subindo arquivo TypeScript do Exercício

---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+// funcao Multiplicar
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+console.log(multiplicar(9, 4));
+
+
+// funcao Saudacao
+function saudacao(nome: string) {
+    return `Olá, ${nome}! Em que posso ajudar?`;
+}
+console.log(saudacao('Fulano'));
